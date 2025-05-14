78f27b844b72ec8a1bf215bddf5a0174ce116de4
Kate <gutkati@email.ru>
2025-05-14 17:42:18 +0400

    Адаптив компонентов

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,77 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './Main.module.scss';
 import ButtonWork from "../buttons/buttonWork/ButtonWork";
 import color from "../../styles/_variables.scss"
 import DayWeek from "../day/DayWeek";
+import {useMediaQuery} from "react-responsive";
 
 const Main = () => {
     const [arrShowDays, setArrShowDays] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+    const isSmallScreen = useMediaQuery({maxWidth: 1120});
+    const isTabletScreen = useMediaQuery({maxWidth: 834});
+    const isMobileScreen = useMediaQuery({maxWidth: 650});
+
+    useEffect(() => {
+        setArrShowDays(getDaysRange())
+    }, [isSmallScreen])
+
+    useEffect(() => {
+        setArrShowDays(getDaysRange())
+    }, [isTabletScreen])
+
+    useEffect(() => {
+        setArrShowDays(getDaysRange())
+    }, [isMobileScreen])
+
+    function getDaysRange() {
+        const today = new Date();
+        const dayOfWeek = today.getDay();
+        const startOfCurrentWeek = new Date(today);
+
+        // Определяем начало текущей недели (понедельник)
+        startOfCurrentWeek.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1))
+
+        // Определяем начало предыдущей недели (понедельник)
+        const startOfPreviousWeek = new Date(startOfCurrentWeek)
+        startOfPreviousWeek.setDate(startOfCurrentWeek.getDate() - 7)
+
+        // Проверяем ширину экрана
+
+        // Количество дней (7 для маленьких экранов, 14 для остальных)
+        const days = []
+
+        if (isMobileScreen) {
+            for (let i = 0; i < 7; i++) {
+                const day = new Date(startOfCurrentWeek)
+                day.setDate(startOfCurrentWeek.getDate() + i)
+
+                days.push(day)
+            }
+        } else if (isTabletScreen) {
+            for (let i = 0; i < 10; i++) {
+                const day = new Date(startOfCurrentWeek)
+                day.setDate(startOfCurrentWeek.getDate() + i)
+
+                days.push(day)
+            }
+        }
+         else if (isSmallScreen) {
+            for (let i = 0; i < 14; i++) {
+                const day = new Date(startOfCurrentWeek)
+                day.setDate(startOfCurrentWeek.getDate() + i)
+
+                days.push(day)
+            }
+        } else {
+            for (let i = 0; i < 18; i++) {
+                const day = new Date(startOfPreviousWeek)
+                day.setDate(startOfPreviousWeek.getDate() + i)
+
+                days.push(day)
+            }
+        }
+        return days
+    }
 
     return (
         <div className={styles.main}>
